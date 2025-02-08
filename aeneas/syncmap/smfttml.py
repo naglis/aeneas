aacfaf135d4f53700ee4951f0c9a90aed7f46526
Naglis Jonaitis <naglis@mailbox.org>
2025-02-08 16:40:55 +0200

    Refactor TTML

--- aeneas/syncmap/smfttml.py
+++ aeneas/syncmap/smfttml.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 # aeneas is a Python/C library and a set of tools
 # to automagically synchronize audio and text (aka forced alignment)
 #
@@ -20,9 +18,19 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import functools
 
 from aeneas.syncmap.smfgxml import SyncMapFormatGenericXML
 import aeneas.globalfunctions as gf
+
+import lxml.etree as ET
+
+TTML_NS = "http://www.w3.org/ns/ttml"
+XML_NS = "http://www.w3.org/XML/1998/namespace"
+
+
+with_ttml_ns = functools.partial(gf.with_ns, ns=TTML_NS)
+with_xml_ns = functools.partial(gf.with_ns, ns=XML_NS)
 
 
 class SyncMapFormatTTML(SyncMapFormatGenericXML):
@@ -37,14 +45,10 @@ class SyncMapFormatTTML(SyncMapFormatGenericXML):
     DEFAULT = TTML
 
     def parse(self, input_text, syncmap):
-        from lxml import etree
-
-        ttml_ns = "{http://www.w3.org/ns/ttml}"
-        xml_ns = "{http://www.w3.org/XML/1998/namespace}"
-        root = etree.fromstring(gf.safe_bytes(input_text))
-        language = root.get(xml_ns + "lang")
-        for elem in root.iter(ttml_ns + "p"):
-            identifier = gf.safe_unicode(elem.get(xml_ns + "id"))
+        root = ET.fromstring(gf.safe_bytes(input_text))
+        language = root.get(with_xml_ns("lang"))
+        for elem in root.iter(with_ttml_ns("p")):
+            identifier = gf.safe_unicode(elem.get(with_xml_ns("id")))
             begin = gf.time_from_ttml(elem.get("begin"))
             end = gf.time_from_ttml(elem.get("end"))
             fragment_lines = self._get_lines_from_node_text(elem)
@@ -58,29 +62,22 @@ class SyncMapFormatTTML(SyncMapFormatGenericXML):
             )
 
     def format(self, syncmap):
-        from lxml import etree
-
         # get language
         language = None
-        if (self.parameters is not None) and ("language" in self.parameters):
+        if self.parameters is not None and "language" in self.parameters:
             language = self.parameters["language"]
         elif len(syncmap.fragments) > 0:
             language = syncmap.fragments[0].text_fragment.language
         if language is None:
             language = ""
 
-        # namespaces
-        ttml_ns = "http://www.w3.org/ns/ttml"
-        xml_ns = "http://www.w3.org/XML/1998/namespace"
-        ns_map = {None: ttml_ns}
-
         # build tree
-        tt_elem = etree.Element("{%s}tt" % ttml_ns, nsmap=ns_map)
-        tt_elem.attrib["{%s}lang" % xml_ns] = language
+        tt_elem = ET.Element(with_ttml_ns("tt"), nsmap={None: TTML_NS})
+        tt_elem.attrib[with_xml_ns("lang")] = language
         # TODO add metadata from parameters here?
-        # COMMENTED head_elem = etree.SubElement(tt_elem, "{%s}head" % ttml_ns)
-        body_elem = etree.SubElement(tt_elem, "{%s}body" % ttml_ns)
-        div_elem = etree.SubElement(body_elem, "{%s}div" % ttml_ns)
+        # COMMENTED head_elem = ET.SubElement(tt_elem, with_ttml_ns("head"))
+        body_elem = ET.SubElement(tt_elem, with_ttml_ns("body"))
+        div_elem = ET.SubElement(body_elem, with_ttml_ns("div"))
 
         if syncmap.is_single_level:
             # single level
@@ -92,23 +89,23 @@ class SyncMapFormatTTML(SyncMapFormatGenericXML):
                     gf.time_to_ttml(fragment.end),
                     "<br/>".join(text.lines),
                 )
-                p_elem = etree.fromstring(p_string)
+                p_elem = ET.fromstring(p_string)
                 div_elem.append(p_elem)
         else:
             # TODO support generic multiple levels
             # multiple levels
             for par_child in syncmap.fragments_tree.children_not_empty:
                 text = par_child.value.text_fragment
-                p_elem = etree.SubElement(div_elem, "{%s}p" % ttml_ns)
+                p_elem = ET.SubElement(div_elem, with_ttml_ns("p"))
                 p_elem.attrib["id"] = text.identifier
                 for sen_child in par_child.children_not_empty:
                     text = sen_child.value.text_fragment
-                    sen_span_elem = etree.SubElement(p_elem, "{%s}span" % ttml_ns)
+                    sen_span_elem = ET.SubElement(p_elem, with_ttml_ns("span"))
                     sen_span_elem.attrib["id"] = text.identifier
                     for wor_child in sen_child.children_not_empty:
                         fragment = wor_child.value
-                        wor_span_elem = etree.SubElement(
-                            sen_span_elem, "{%s}span" % ttml_ns
+                        wor_span_elem = ET.SubElement(
+                            sen_span_elem, with_ttml_ns("span")
                         )
                         wor_span_elem.attrib["id"] = fragment.text_fragment.identifier
                         wor_span_elem.attrib["begin"] = gf.time_to_ttml(fragment.begin)
