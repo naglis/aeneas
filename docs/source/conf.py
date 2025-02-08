0d5ecabbf26530642bafa07d1bca69d974830433
Naglis Jonaitis <naglis@mailbox.org>
2025-02-08 09:28:34 +0200

    Update docs config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,7 +41,7 @@ extensions = [
 templates_path = ["_templates"]
 
 # The suffix of source filenames.
-source_suffix = ".rst"
+source_suffix = {".rst": "restructuredtext"}
 
 # The encoding of source files.
 # source_encoding = 'utf-8-sig'
@@ -67,7 +67,7 @@ release = "1.7.3"
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
